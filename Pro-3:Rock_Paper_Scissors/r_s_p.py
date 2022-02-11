37baac8c0116bd2d84e5614fdee066e3221a42a7
wmt-krunald <krunald@webmob.tech>
2022-02-11 14:38:03 +0530

    Rock, Paper and Scissors Game

---
+++ Pro-3:Rock_Paper_Scissors/r_s_p.py
@@ -0,0 +1,19 @@
+import random
+
+def play_rsp():
+    user = input("\nWhat is your Choice ?\n 'r' for Rock,\n 's' for Scissors,\n 'p' for Paper\n")
+    comp = random.choice(['r','s','p'])
+
+    if user == comp:
+        return "It's tie."
+    
+    if is_win(user, comp):
+        return "\nHurreeyyy, You Win this Match."
+    
+    return "\nBetter Luck For Next Time.."
+#when user win : if -> r > s, s > p, p > r.
+def is_win(player, computer):   #This function return True if player will win.
+    if (player == 'r' and computer == 's') or (player == 's' and computer == 'p') or (player == 'p' and computer == 'r'):
+        return True
+
+print(play_rsp())
