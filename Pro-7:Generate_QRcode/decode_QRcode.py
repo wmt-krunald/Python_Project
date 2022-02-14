7a04e9d222a046ea8a0dabaad05ca9ac43fc54ff
wmt-krunald <krunald@webmob.tech>
2022-02-14 16:40:09 +0530

    Decode the QRcode and Collect the Data

--- Pro-7:Generate_QRcode/decode_QRcode.py
+++ Pro-7:Generate_QRcode/decode_QRcode.py
@@ -2,11 +2,13 @@ import cv2
 
 detector = cv2.QRCodeDetector()
 
-revel,point,s_qr = detector.detectAndDecode(cv2.imread("/home/webmob/Krunal_Work_space/Python/Python_Project/Pro-7:Generate_QRcode/2.png"))
-print("\n", revel)
+data,point,s_qr = detector.detectAndDecode(cv2.imread("/home/webmob/Krunal_Work_space/Python/Python_Project/Pro-7:Generate_QRcode/2.png"))
+print("\n", data)
 
 print("\nPoints: ")
 print("\n", point)
+
+print("\n", s_qr)
 
 # img = Image.open("/home/webmob/Krunal_Work_space/Python/Python_Project/Pro-7:Generate_QRcode/1.png")
 
